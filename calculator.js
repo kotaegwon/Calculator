15275177ae3bdbfc507083e2ee2d48cc874232d5
kotaegwon <rhxornjs@naver.com>
2022-02-15 18:05:32 +0900

    node express 계산기

--- calculator.js
+++ calculator.js
@@ -1,8 +1,25 @@
-var express=require('express');
+const express=require('express');
+const bodyParser=require('body-parser');
 var app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+// app.get("/",(req,res)=>{
+//     res.send("asd");
+// });
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/view/cal.html")
 })
 
-app.listen(3000);
+// localhost:3000/main 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
+app.get('/view', function(req,res) {
+    res.sendFile(__dirname + "/view/cal.html")
+})
+
+app.post("/",(req,res)=>{
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var result=num1+num2;
+    res.send("입력한 두 수의 합 : "+result);
+});
+
+app.listen(7777);
